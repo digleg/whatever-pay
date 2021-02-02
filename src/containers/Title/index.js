d74131817b96045f3d11f14b6e2a69cbafd217b3
vito huang <digleg_h@hotmail.com>
2021-02-02 14:13:39 +0800

    [title] fix img i18n showing problem

--- src/containers/Title/index.js
+++ src/containers/Title/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import logoBig from '../../images/logo_big.png';
-import downloadEN from '../../images/download_cn.png';
+import downloadEN from '../../images/download_en.png';
 import downloadCN from '../../images/download_cn.png';
 import './styles.css';
 
